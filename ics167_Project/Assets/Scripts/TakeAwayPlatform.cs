fe3d89fa390c49e0f995a85b5865b5e6d8fd21ab
huaac <huaac1@uci.edu>
2022-01-31 00:01:36 -0800

    Finished Test Level 1 and added hole art

---
+++ ics167_Project/Assets/Scripts/TakeAwayPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeAwayPlatform : MonoBehaviour
+{
+    public GameObject temp_collider;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //if trigger catches player, then disable temp_collider
+        if(col.gameObject.tag == "Player")
+        {
+            temp_collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+        }
+    }
+}
