df7c34773785c41a62616b36408927ec866dd080
Aissa Akiyama <arcticamaroq@gmail.com>
2022-02-14 15:31:21 -0800

    temporarily removed .unitypackage from .gitignore
    to be able to add in NavMeshPlus as a package

---
+++ ics167_Project/Assets/Scripts/EnemyDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    [SerializeField] private Transform searcher;
+    private GameObject enemy;
+
+    private void Update()
+    {
+        gameObject.transform.position = searcher.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.gameObject.tag == "Enemy") && (enemy == null))
+        {
+            enemy = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemy = null;
+        }
+    }
+
+    public GameObject EnteredEnemy
+    {
+        get { return enemy; }
+    }
+}
---
+++ ics167_Project/Assets/Scripts/FSM/FollowAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "FSM/Actions/Follow", order = 53)]
+public class FollowAction : Action
+{
+    public override void Act(HelperFSM machine)
+    {
+        machine.transform.position = machine.HomePosition;
+    }
+}
