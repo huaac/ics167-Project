adc57aeee926f8285f8a0bfdac8012a574e57ecb
Aissa Akiyama <arcticamaroq@gmail.com>
2022-02-14 16:29:11 -0800

    added initial helper ghost
    - currently only follows player and seeks enemies, does not attack yet

---
+++ ics167_Project/Assets/Scripts/FSM/FoundEnemyDecision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "FSM/Decisions/Found Enemy")]
+public class FoundEnemyDecision : Decision
+{
+    public override bool Decide(HelperFSM machine)
+    {
+        GameObject possibleEnemy = machine.SearchRange.GetComponent<EnemyDetector>().EnteredEnemy;
+
+        return possibleEnemy != null;
+    }
+}
