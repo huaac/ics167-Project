2e2578e84fc4b2c6d74639bc6395cbb0b342e022
huaac <huaac1@uci.edu>
2022-02-19 21:19:06 -0800

    accidentally got rid of navmesh and tried adding more spider functions

--- ics167_Project/Assets/Scripts/Enemy Scripts/RatTrap.cs
+++ ics167_Project/Assets/Scripts/Enemy Scripts/RatTrap.cs
@@ -10,13 +10,30 @@ public class RatTrap : KillableEnemy
 {
     Animator myAnimator;
 
-    public override void OnCollisionEnter2D(Collision2D col)
+    // void FixedUpdate()
+    // {
+    //     // rn it only detects if mouse is straight up in the middle of the trap (FIX LATER)
+    //     // not sure if I should continue using raycast or use use colliders
+    //     RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up),0); //LayerMask.GetMask("Player"));
+
+    //     if(hit.collider)
+    //     {
+    //         Attack();
+    //     }
+    // }
+
+    public override void Attack()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetTrigger("Touched");
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
     {
         // if player toucches the rat trap, rat trap animation and they fall down, then reset scene
         if(col.gameObject.tag == "Player")
         {
-            myAnimator = GetComponent<Animator>();
-            myAnimator.SetTrigger("Touched");
+            Attack();
             //col.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             //TakeDamage(50);
             //CallResetScene();
--- ics167_Project/Assets/Scripts/Enemy Scripts/Spider.cs
+++ ics167_Project/Assets/Scripts/Enemy Scripts/Spider.cs
@@ -14,38 +14,26 @@ public class Spider : KillableEnemy
     public Transform detect_ground;
     public float speed;
     private bool facing_right;
-    //public float distance;
+    private float spider_wait_time;
 
     private void Awake()
     {
+        //speed neg bc spider is facing right
+        spider_wait_time =2f;
         speed = -4f;
         facing_right = true;
         animator.SetFloat("speed", 1);
+        animator.SetBool("isDetected", false);
         //m_sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        RaycastHit2D groundInfo = Physics2D.Raycast(detect_ground.position, Vector2.down, LayerMask.GetMask("jumpableGround"));
-        if(groundInfo.collider == false)
+        if(SeePrey())
         {
-            if(facing_right)
-            {
-                //m_sprite.flipX = true;
-                transform.eulerAngles = new Vector3(0,-180,0);
-                facing_right = false;
-                //speed = -speed;
-            }
-            else
-            {
-                //m_sprite.flipX = false;
-                transform.eulerAngles = new Vector3(0,0,0);
-                facing_right = true;
-                //speed = -speed;
-            }
-            //animator.SetBool("isDetected", true);
-            //animator.SetFloat("speed", 1);
+            Chase();
         }
+        else{Patrol();}
     }
 
     void FixedUpdate()
@@ -53,17 +41,83 @@ public class Spider : KillableEnemy
         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
-    public override void OnCollisionEnter2D(Collision2D col)
+    private void Patrol()
     {
-        // if player touches spider, player falls down and reset scene
-        /*
-        if(col.gameObject.tag == "Player")
+        RaycastHit2D groundInfo = Physics2D.Raycast(detect_ground.position, Vector2.down, LayerMask.GetMask("jumpableGround"));
+        if(groundInfo.collider == false)
         {
-            col.gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            TakeDamage(50);
-            CallResetScene();
+            // rn the spider immediately turns around when it reaches an edge.
+            // should this be changed so that it doesnt turn until it moves again?
+            StartCoroutine(WaitTime());
+            if(facing_right)
+            {
+                //m_sprite.flipX = true;
+                transform.eulerAngles = new Vector3(0,-180,0);
+                facing_right = false;
+            }
+            else
+            {
+                //m_sprite.flipX = false;
+                transform.eulerAngles = new Vector3(0,0,0);
+                facing_right = true;
+            }
         }
-        */
     }
+
+    IEnumerator WaitTime()
+    {
+        speed = 0;
+        animator.SetFloat("speed", 0);
+        yield return new WaitForSeconds(spider_wait_time);
+        speed = -4f;
+        animator.SetFloat("speed", 1);
+    }
+
+    private bool SeePrey()
+    {
+        RaycastHit2D detected = Physics2D.Raycast(detect_ground.position, transform.TransformDirection(Vector2.left),5,LayerMask.GetMask("Player"));
+        if(detected.collider){return true;}
+        else {return false;}
+
+    }
+
+    IEnumerator Alerted()
+    {
+        speed = 0;
+        animator.SetFloat("speed", 0);
+        animator.SetBool("isDetected", true);
+        yield return new WaitForSeconds(spider_wait_time);
+        animator.SetBool("isDetected", false);
+        speed = -6f;
+        animator.SetFloat("speed", 1);
+    }
+
+    private void Chase()
+    {
+        StartCoroutine(Alerted());
+        speed = -6f;
+    }
+
+    public override void Attack()
+    {
+    }
+
+    //animator.SetBool("isDetected", true);
+    //animator.SetFloat("speed", 1);
+
+    // public override void OnCollisionEnter2D(Collision2D col)
+    // {
+    //     // if player touches spider, player falls down and reset scene
+    //     /*
+    //     if(col.gameObject.tag == "Player")
+    //     {
+    //         col.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    //         TakeDamage(50);
+    //         CallResetScene();
+    //     }
+    //     */
+    // }
+
+    
 
 }
