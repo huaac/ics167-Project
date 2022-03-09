5e1b469de9e5acdf442e17826db8ee998386ac52
Mindy Jun <mindyjvn@gmail.com>
2022-03-08 17:14:20 -0800

    Basic BGM
    Implemented Basic BGM's, Audio Manager not working
    
    need to implement jump, chew, powerup, finish sound bits

---
+++ ics167_Project/Assets/Scripts/Audio/BGSoundScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// by Mindy Jun
+public class BGSoundScript : MonoBehaviour
+{
+    private static BGSoundScript instance;
+    public static BGSoundScript Instance
+    {
+        get { return instance; }
+    }
+
+    public AudioSource BGM;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update() 
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.name == "TutorialLevel")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Stop() 
+    {
+        BGM.Stop();
+    }
+
+    public void ChangeBGM(AudioClip music) 
+    {
+        BGM.Stop();
+        BGM.clip = music;
+        BGM.Play();
+    }
+
+}
