96cde4f9b5e2df8dbf3e2a8900b938e58efcd4e2
Aissa Akiyama <arcticamaroq@gmail.com>
2022-03-09 17:22:10 -0800

    added a scene with a crude boss fight
    still has many bugs

---
+++ ics167_Project/Assets/Boss_ChargeState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_ChargeState : StateMachineBehaviour
+{
+    [SerializeField] private float m_speed = 2.5f;
+    private float left;
+    private float right;
+
+    private Boss boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss = animator.GetComponent<Boss>();
+        left = boss.Left.position.x;
+        right = boss.Right.position.x;
+
+        boss.StartChargeTimer();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.transform.position = new Vector2(
+            Mathf.PingPong(Time.time * m_speed, right - left) + left,
+            boss.transform.position.y);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("faint");
+        boss.ReleasePowerUps();
+    }
+}
---
+++ ics167_Project/Assets/Scripts/Enemy Scripts/Boss.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : KillableEnemy
+{
+    [SerializeField] private Transform left;
+    [SerializeField] private Transform right;
+    [SerializeField] private float chargeLength = 1f;
+    [SerializeField] private float faintLength = 20f;
+    private float movement_x;
+    private Vector2 previousPosition;
+
+    [SerializeField] private GameObject protein;
+    [SerializeField] private float offset = 100f;
+
+    private Rigidbody2D m_rb;
+    private BoxCollider2D m_col;
+    private SpriteRenderer m_sprite;
+    private Animator m_anim;
+
+    private void Awake()
+    {
+        base.Awake();
+
+        m_rb = GetComponent<Rigidbody2D>();
+        m_col = GetComponent<BoxCollider2D>();
+        m_sprite = GetComponent<SpriteRenderer>();
+        m_anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        m_anim.SetFloat("chargeLength", 20f);
+        m_anim.SetFloat("faintLength", 20f);
+    }
+
+    private void Update()
+    {
+        // flip sprite according to velocity
+        if (movement_x >= 0f)
+        {
+            m_sprite.flipX = false;
+        }
+        else
+        {
+            m_sprite.flipX = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        movement_x = transform.position.x - previousPosition.x;
+        previousPosition = transform.position;
+    }
+
+    public void StartChargeTimer()
+    {
+        StartCoroutine(ChargeTimer());
+    }
+    private IEnumerator ChargeTimer()
+    {
+        float timer = chargeLength;
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timer--;
+        }
+
+        m_anim.SetTrigger("faint");
+    }
+
+    public void ReleasePowerUps()
+    {
+        GameObject go = (GameObject)Instantiate(protein,
+            new Vector3(transform.position.x + offset,
+                        transform.position.y,
+                        0f),
+            Quaternion.identity);
+
+        go.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 0f);
+
+        GameObject go2 = (GameObject)Instantiate(protein,
+            new Vector3(transform.position.x - offset,
+                        transform.position.y,
+                        0f),
+            Quaternion.identity);
+
+        go2.GetComponent<Rigidbody2D>().velocity = new Vector2(-2f, 0f);
+    }
+
+    public void StartFaintTimer()
+    {
+        StartCoroutine(FaintTimer());
+    }
+    private IEnumerator FaintTimer()
+    {
+        float timer = faintLength;
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timer--;
+        }
+
+        m_anim.SetTrigger("wakeUp");
+    }
+
+
+    public Transform Left { get { return left; } }
+    public Transform Right { get { return right; } }
+}
